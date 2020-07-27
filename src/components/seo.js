2f18845c156417d1d287e85660dce5ad923b32e5
Toni Castillo <tonicastillo@gmail.com>
2020-07-27 13:43:17 +0200

    google font fix

--- src/components/seo.js
+++ src/components/seo.js
@@ -68,7 +68,9 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Montserrat:wght@300;500;600&display=swap" rel="stylesheet"/>
+    </Helmet>
   )
 }
 
